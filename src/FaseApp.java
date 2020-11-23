b77e86a29a97b9077d1c6d3d2af1e11f6d3fbd2a
StefanoPlaFarg <stefano.pola.fargas@gmail.com>
2020-11-23 13:13:10 +0100

    Branca: Fase2

--- src/FaseApp.java
+++ src/FaseApp.java
@@ -1,22 +1,20 @@
+import java.util.Scanner;
+import java.util.*;
+
 /*
-Crea sis variables tipu string buides.
-Demana per consola que s’introdueixin els noms. 
-Introdueix els següents noms de ciutats (Barcelona, Madrid, Valencia, Malaga, Cadis, Santander) per teclat.
-Mostra per consola el nom de les 6 ciutats.
+ Un cop tenim els noms de les ciutats guardats en variables haurem de pasar l’informacio a un array (arrayCiutats).
+Quan tinguem l’array ple, haurem de mostrar per consola el nom de les ciutats ordenadas per ordre alfabetic.
+
  * 
  * 
  */
-
-
-
-import java.util.Scanner;
-
 public class FaseApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 		String var1,var2,var3,var4,var5,var6;
+		ArrayList<String> arrayCiutats = new ArrayList<String>();
 		Scanner myObj = new Scanner(System.in);
 		System.out.println("Introdueix 6 noms de ciutats\n" );		
 		var1=myObj.nextLine();
@@ -26,6 +24,16 @@ public class FaseApp {
 		var5=myObj.nextLine();
 		var6=myObj.nextLine();
 		System.out.println("Noms introduïts");
+		arrayCiutats.add(var1);
+		arrayCiutats.add(var2);
+		arrayCiutats.add(var3);
+		arrayCiutats.add(var4);
+		arrayCiutats.add(var5);
+		arrayCiutats.add(var6);
+		Collections.sort(arrayCiutats);		
+		System.out.println(arrayCiutats);
+
 	}
+	
 
 }
